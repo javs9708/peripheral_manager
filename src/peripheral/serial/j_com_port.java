4dcba03415f03f920c051fb4f8634a7ce9220d44
Alexander Villegas <villegassanchezjhonalexander@gmail.com>
2019-05-09 14:56:40 -0500

    1.5

--- src/peripheral/serial/j_com_port.java
+++ src/peripheral/serial/j_com_port.java
@@ -692,6 +692,7 @@ public class j_com_port implements SerialPortDataListener
 
     for (SerialPort serial_port : ports)
     {
+        System.out.println(serial_port.getSystemPortName());
       if (serial_port.getSystemPortName().equals(comport_name))
       {
         debug.set_debug(comport_name + " Present");
